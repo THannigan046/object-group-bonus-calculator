740350ca63d8c574342400e6bd67490a69504baf
Toivo Hannigan <THannigan046@gmail.com>
2021-12-07 13:34:25 -0600

    completed group bonus calculator yaaaay

--- client.js
+++ client.js
@@ -31,6 +31,7 @@ const employees = [
   }
 ];
 
+
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
 // This problem is massive! Break the problem down, take small steps, and test as you go.
@@ -40,3 +41,40 @@ const employees = [
 // Ask questions when you don't.
 
 console.log( employees );
+for (zeep of employees) {
+  console.log(zeep);
+};
+function EmployeeMaker(employee) {
+  this.name = employee.name;
+  bonusPercentage = 0;
+  //console.log('what is the review rating', employee.reviewRating);
+  if (employee.reviewRating === 2) {
+    bonusPercentage = 0;
+  }
+  else if (employee.reviewRating === 3) {
+    bonusPercentage = 0.04;
+    //console.log('in line 53', bonusPercentage, employee.reviewRating);
+  }
+  else if (employee.reviewRating === 4) {
+    bonusPercentage = 0.06;
+  }
+  else if (employee.reviewRating === 5) {
+    bonusPercentage = 0.1;
+  }
+  else if (employee.employeeNumber >= 1000) {
+    bonusPercentage = bonusPercentage + 0.05;
+  }
+  else if (employee.annualSalary > 65000){
+    bonusPercentage = bonusPercentage - 0.01;
+  }
+  if (bonusPercentage > 0.13, bonusPercentage < 0){
+    bonusPercentage = undefined;
+  }
+  this.bonusPercentage = bonusPercentage,
+  this.totalCompensation = employee.annualSalary + bonusPercentage,
+  this.totalBonus = Math.round(this.totalCompensation);
+
+};
+console.log(new EmployeeMaker(employees[0]));
+console.log(new EmployeeMaker(employees[1]));
+
